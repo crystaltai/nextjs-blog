b45d348725c163c3f8db922d94a4dc5e05295cd1
Crystal Tai <crystaltai315@gmail.com>
2023-01-19 21:51:04 -0800

    feat: render markdown file content

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,5 +1,6 @@
 import Layout from '../../components/layout';
 import { getAllPostsIds, getPostData } from '../../lib/posts';
+import Head from 'next/head';
 
 export async function getStaticPaths() {
   const paths = getAllPostsIds();
@@ -12,7 +13,7 @@ export async function getStaticPaths() {
 
 // Uses getPostData() in getStaticProps() to get the post data and return it as props
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  const postData = await getPostData(params.id); // Need to add await because getPostData() uses await
   return {
     props: {
       postData,
@@ -23,11 +24,16 @@ export async function getStaticProps({ params }) {
 export default function Post({ postData }) {
   return (
     <Layout>
-      {postData.title}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
       <br />
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      {/* This is React's replacement for using innerHTML in the browser DOM */}
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   );
 }
